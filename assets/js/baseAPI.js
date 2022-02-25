837ab168d4a4a1cb6c06d2d4777ed204f3dac30b
Luqiang07 <976057894@qq.com>
2022-02-25 19:31:05 +0800

    完成主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,20 @@
 //在这个函数中,可以难倒我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    //统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        };
+    }
+    //全局统一挂在 complete 回调函数
+    options.complete =function (res) {
+        //在complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制清空token
+            localStorage.removeItem('token');
+            //强制跳转到登录页面
+            location.href = '/login.html';
+        }
+    }
 })
