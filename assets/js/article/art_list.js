0b3ecf5dfa3294f5e65a12e9bc9cf8df8f4fa961
luqiang <976057894@qq.com>
2022-03-09 17:34:32 +0800

    修改了art_list.html/js文件

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,6 @@
 $(function () {
     var layer = layui.layer;
-
+    var form = layui.form;
 
     //定义美化时间的过滤器
     template.defaults.imports.dataFormat = function (date) {
@@ -30,8 +30,9 @@ $(function () {
         state: ''//文章发布状态
     };
 
-    
+
     initTable();
+    initCate();
     //获取文章列表数据的方法
     function initTable() {
         $.ajax({
@@ -48,4 +49,34 @@ $(function () {
             }
         })
     }
+
+    //初始化文章分类的方法
+    function initCate() {
+        $.ajax({
+            method:'GET',
+            url:'/my/article/cates',
+            success:function(res) {
+                if(res.status !== 0) {
+                    return layer.msg('获取分类数据失败!')
+                }
+                //调用模板引擎渲染分类可选项
+               var htmlStr = template('tpl-cate',res);
+               $('[name=cate_id]').html(htmlStr);
+               //通知layui重新渲染表单区域的UI结构
+               form.render();
+            }
+        })
+    }
+    //为筛选表单绑定 submit 事件
+    $('#form-search').on('submit',function(e) {
+        e.preventDefault();
+        //获取表单中选项的值
+        var cate_id = $('[name=cate_id]').val();
+        var state = $('[name=state]').val();
+        q.cate_id=cate_id;
+        q.state = state;
+        //根据最新筛选条件重新渲染页面
+        initTable();
+    })
+
 })
